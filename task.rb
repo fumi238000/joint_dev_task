7a11e47b268e39233f25b9c8cdd08695d0eadb39
fumi238000 <s21231128@gmail.com>
2020-10-02 18:41:43 +0900

    Q7.Q8,Q10の再回答

--- task.rb
+++ task.rb
@@ -78,7 +78,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array = array.map {|a|a.to_i}
+  array = array.map(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -91,8 +91,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map!{|p| p.capitalize}
-  upper_case_programming_languages = programming_languages.map{|p| p.upcase}  
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase) 
  
   # 以下は変更しないで下さい
   p programming_languages
@@ -120,12 +120,12 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    if food.include?("うに") == true 
-     puts "好物です"
-  else 
-    puts "まぁまぁ好きです"
+    if food.include?("うに")
+      puts "好物です"
+    else 
+      puts "まぁまぁ好きです"
+    end
   end
- end
 end
 
 
