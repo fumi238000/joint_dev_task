75ba60c9009dc46d09ba1c655f320e23a3f4e6b8
fumi238000 <s21231128@gmail.com>
2020-10-03 13:58:03 +0900

    Ruby基礎課題Q15,Q16再提出

--- task.rb
+++ task.rb
@@ -179,8 +179,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  puts (data1.has_key?(:age)? "OK": "NO")
-  puts (data2.has_key?(:age)? "OK": "NO")
+  puts data1.has_key?(:age)? "OK": "NO"
+  puts data2.has_key?(:age)? "OK": "NO"
 end
 
 
@@ -195,7 +195,9 @@ def q16
   ]
 
   # 以下に回答を記載
-  users.each {|user| puts "私の名前は#{user[:name]}です。\n年齢は#{user[:age]}です。"}
+  users.each do |user| 
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end 
 end
 
 
